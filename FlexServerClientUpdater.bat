ecfc5aeef04c372c32e1dd52f8876921a1318b03
adhdheytheresasquirrel <kevin.decker@motorolasolutions.com>
2022-08-22 21:26:44 -0400

    Update FlexServerClientUpdater.bat
    Added brief explanation in the bat file for how the utility works.

--- FlexServerClientUpdater.bat
+++ FlexServerClientUpdater.bat
@@ -1,5 +1,9 @@
 @echo
+
+### This is the Flex Server Client Updater configuration file. Here you will specify what you want the utility to locate & replace within the PC's configuration. It is important to run this utility as an administrator on the PC or it will not update the registry keys for the Mobile client.
+###
+
 "fnr.exe" --cl --dir "%ProgramData%\Spillman\Spillman" --fileMask "comdflts.txt" --find "http\://10.209.0.159\:4080" --replace "https\://njmonso.spillman.agency\:4443"
-"fnr.exe" --cl --dir "%ProgramData%\Spillman\Spillman" --fileMask "comdflts.txt" --find "https\://10.209.0.159\:4443" --replace "https\://njmonso.spillman.agency\:4443" 
+"fnr.exe" --cl --dir "%ProgramData%\Spillman\Spillman" --fileMask "comdflts.txt" --find "https\://10.209.0.159\:4443" --replace "https\://njmonso.spillman.agency\:4443"
 reg add HKCU\Software\Spillman\Mobile\Settings /v SpillmanPort /t REG_DWORD /d 4443 /f
 reg add HKCU\Software\Spillman\Mobile\Settings /v HostAddress /t REG_SZ /d njmonso.spillman.agency /f
